22674c18b0d7f033840da736647eb03e1ab7caf8
Cyril Gautray <gautray.cyril@hotmail.com>
2019-02-01 14:30:01 +0100

    gratuit grtuit

--- src/fr/ensup/avenger/dao/AccesBd.java
+++ src/fr/ensup/avenger/dao/AccesBd.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class AccesBd {
-
+//irugheqirsghreifdsugheirgheqigheqirtghqdskighrqituhretiersq
 	public Connection cn;
 	public Statement st;
 
